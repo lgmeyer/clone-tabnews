ff0004bbe419912014aff9aff5eddf3365d678c4
Leandro Meyer <leandro.meyer@gmail.com>
2024-11-05 20:47:20 +0000

    make database consult dynamic

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -8,7 +8,11 @@ async function status(req, res) {
   const maxConnectionsResult = await database.query('SHOW max_connections;');
   const maxConnectionsValue = parseInt(maxConnectionsResult.rows[0].max_connections, 10);
 
-  const openedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'postgres';");
+  const dataBaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dataBaseName]
+  });
   const openedConnectionsValue = openedConnectionsResult.rows[0].count
 
 
