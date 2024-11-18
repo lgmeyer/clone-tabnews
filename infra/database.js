2f1056b34f615e03904693e6c9161ff2d66b7f32
Leandro Meyer <leandro.meyer@gmail.com>
2024-11-18 14:34:04 +0000

    adds migration scripts

--- infra/database.js
+++ infra/database.js
@@ -11,13 +11,6 @@ async function query(queryObject) {
     ssl: process.env.NODE_ENV === 'development' ? false : true 
   });
 
-  console.log(    
-    process.env.POSTGRES_HOST,
-    process.env.POSTGRES_PORT,
-    process.env.POSTGRES_USER,    
-    process.env.POSTGRES_DB,
-    process.env.POSTGRES_PASSWORD, )
-
   try {
     await client.connect();
     const result = await client.query(queryObject);
