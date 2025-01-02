c50861aa046dbe7b774c23e02c24d7eff6c47d59
Leandro Meyer <leandro.meyer@gmail.com>
2025-01-02 17:07:32 +0000

    ajusta configuração do Jest para carregar variáveis de ambiente e adiciona limpeza do banco de dados nos testes de migrações

--- tests/integrations/api/v1/migrations/get.test.js
+++ tests/integrations/api/v1/migrations/get.test.js
@@ -1,5 +1,10 @@
 import database from "infra/database.js"
 
+beforeAll(cleanDataBase);
+async function cleanDataBase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("GET /api/v1/migrations sould return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
